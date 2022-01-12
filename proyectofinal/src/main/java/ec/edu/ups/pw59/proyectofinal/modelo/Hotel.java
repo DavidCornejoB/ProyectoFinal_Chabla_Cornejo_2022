b716f45894601ddc5f4cd5ab7532aaf7d318b594
Elwilliko <52544334+Elwilliko@users.noreply.github.com>
2022-01-12 18:38:28 -0500

    Creacion modelo
    Creacion de habitacion, reserva, categoria, hotel

---
+++ proyectofinal/src/main/java/ec/edu/ups/pw59/proyectofinal/modelo/Hotel.java
@@ -0,0 +1,91 @@
+package ec.edu.ups.pw59.proyectofinal.modelo;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Hotel implements Serializable{
+	
+	@Id
+	@Column(length = 20, name = "ho_codigo")
+	private int codigo;
+	
+	@Column(name = "ho_nombre")
+	private String nombre;
+	
+	@Column(name = "ho_pais")
+	private String pais;
+	
+	@Column(name = "ho_provicia")
+	private String provincia;
+	
+	@Column(name = "ho_ciudad")
+	private String ciudad;
+	/**
+	 * @return the codigo
+	 */
+	public int getCodigo() {
+		return codigo;
+	}
+	/**
+	 * @param codigo the codigo to set
+	 */
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the pais
+	 */
+	public String getPais() {
+		return pais;
+	}
+	/**
+	 * @param pais the pais to set
+	 */
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	/**
+	 * @return the provincia
+	 */
+	public String getProvincia() {
+		return provincia;
+	}
+	/**
+	 * @param provincia the provincia to set
+	 */
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	/**
+	 * @return the ciudad
+	 */
+	public String getCiudad() {
+		return ciudad;
+	}
+	/**
+	 * @param ciudad the ciudad to set
+	 */
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	
+	
+	
+
+}
