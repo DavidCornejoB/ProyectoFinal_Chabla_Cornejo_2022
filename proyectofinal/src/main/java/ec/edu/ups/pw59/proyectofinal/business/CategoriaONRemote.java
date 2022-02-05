180fc04ad62b66085c20712f8ee1f47167ac66ad
DavidCornejoB <33791162+DavidCornejoB@users.noreply.github.com>
2022-02-04 19:38:10 -0500

    Correcciones ON Categoria
    Se han corregido parámetros de entrada en los métodos crud de categoriaON

--- proyectofinal/src/main/java/ec/edu/ups/pw59/proyectofinal/business/CategoriaONRemote.java
+++ proyectofinal/src/main/java/ec/edu/ups/pw59/proyectofinal/business/CategoriaONRemote.java
@@ -14,7 +14,7 @@ public interface CategoriaONRemote {
 	
 	public void update(Categoria c) throws Exception;
 	
-	public void read(int id) throws Exception;
+	public Categoria read(int id) throws Exception;
 	
 	public void delete(int id) throws Exception;
 	
